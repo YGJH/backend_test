d601c3f8efc7d1fe69bbe2fa90fb4a21265f03f8
YGJH <jihuty@gmail.com>
2024-12-13 12:12:01 +0800

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 const apiUrl = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EBC821F3-9782-4630-8E87-87FF25933C15';
 const server = http.createServer((req, res) => {
   fetch(apiUrl).then((response) => response.json()).then((data) => {
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   // console.log('Server is running on http://localhost:3000');
 });
