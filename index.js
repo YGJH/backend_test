791e3432b86fd6dac269a57151ff23c83adad9a8
YGJH <jihuty@gmail.com>
2024-12-13 13:58:01 +0800

    http

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-import cors from 'cors';
-import express from 'express';
+import http from 'http';
 import fs from 'fs';
 import fetch from 'node-fetch';
 
-const app = express();
+const apiUrl = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EBC821F3-9782-4630-8E87-87FF25933C15';
 
-// 啟用所有來源的 CORS
-app.use(cors());
+const server = http.createServer((req, res) => {
+    // 設置 CORS 頭部
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-const apiUrl =
-    'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EBC821F3-9782-4630-8E87-87FF25933C15';
+    if (req.method === 'GET' && req.url === '/') {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    fs.writeFileSync('weather.json', JSON.stringify(data, null, 2));
+                }
+                fs.readFile('weather.json', 'utf8', (err, data) => {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Internal Server Error');
+                    } else {
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end(data);
+                    }
+                });
+            })
+            .catch(err => {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Failed to fetch data');
+            });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
 
-app.get(
-    '/',
-    (req, res) => {fetch(apiUrl)
-                       .then(response => response.json())
-                       .then((data) => {
-  if (data.success) {
-    fs.writeFileSync('weather.json', JSON.stringify(data, null, 2));
-  }
-                       })
-                         fs.readFile('weather.json', 'utf8', (err, data) => {
-  res.end(data);
-                       })});
-
-                         const port = process.env.PORT || 3000;
-                         app.listen(port, () => {
-                           console.log(`伺服器正在監聽埠口 ${port}`);
-                         });
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`伺服器正在監聽埠口 ${port}`);
+});
